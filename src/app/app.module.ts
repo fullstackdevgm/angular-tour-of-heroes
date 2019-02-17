41943533d8e0480536bc6bf21caa5783fb09e929
Full Stack <37483332+fullstackdevgm@users.noreply.github.com>
2019-02-17 15:41:53 +0800

    Task 2 | Routing (#19)
    * Done for first
    
    * bug fix
    
    * update style
    
    * Second Task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroComponent } from './hero/hero.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroComponent }  from './hero/hero.component';
+import { HeroesComponent }      from './heroes/heroes.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
-    HeroComponent
+    HeroComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
